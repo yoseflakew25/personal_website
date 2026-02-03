4340e1f41b610b86673b90d551b1e22ba81c3f84
yosef <akil-vercel@a2sv.org>
2026-02-03 18:59:45 +0300

    feat: Introduce NavList component with a resume download link and include resume PDF.

--- src/components/layout/nav/nav-list.tsx
+++ src/components/layout/nav/nav-list.tsx
@@ -15,6 +15,15 @@ const NavList: React.FC<NavProps> = ({ setOpen }) => {
       {navData.map(nav => (
         <NavItem key={nav.id} setOpen={setOpen} {...nav} />
       ))}
+      <li role="listitem" className="sm:ml-2">
+        <a
+          href="/Yosef Lakew Resume.pdf"
+          download
+          className="px-4 py-1.5 border-2 border-ring text-ring font-medium rounded-full hover:bg-ring hover:text-white transition-all duration-300 text-sm sm:text-base el-focus-styles"
+        >
+          Resume
+        </a>
+      </li>
     </ul>
   )
 }
